c1f27ef2ca3e059b0e7f0e6aa9e129ecdac2e956
Marcos Gutierrez <gmarcos87@gmail.com>
2020-04-14 09:35:51 -0300

    add dependencies and change metro config

---
+++ types/react-native-button/index.d.ts
@@ -0,0 +1,22 @@
+declare module 'react-native-button' {
+    import { Component } from "react";
+
+    type ButtonProps = {
+        allowFontScaling?: boolean;
+        accessibilityLabel?: string;
+        containerStyle?: object;
+        disabledContainerStyle?: object;
+        disabled?: boolean;
+        style?: object;
+        styleDisabled?: object;
+        childGroupStyle?: object;
+        onPress: ()=>void;
+    }
+
+    class Button<P = {}, S = {}> extends Component<ButtonProps, S> {
+        constructor(props: object);
+    };
+
+    export = Button;
+
+};
