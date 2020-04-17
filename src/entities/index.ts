8291e62b29bebaf6d44e837274054725ea3d48b1
Marcos Gutierrez <gmarcos87@gmail.com>
2020-04-17 10:20:44 -0300

    fix typeorm bug

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,1 +1,53 @@
-export { User } from './User';
+import {Entity, Column, PrimaryGeneratedColumn, OneToMany, ManyToOne} from "typeorm/browser";
+
+// USER ENTITY AND TYPES 
+@Entity("user")
+export class User {
+
+    @PrimaryGeneratedColumn({ name: 'id', type: 'integer'})
+    id?: number;
+
+    @Column({ name: 'name', type: 'varchar', nullable: false })
+    name!: string;
+
+    @Column({ name: 'celsius', type: 'boolean', nullable: false })
+    celsius!: boolean;
+
+    @OneToMany(type => Symptom, symptom => symptom.user)
+    symptoms?: Symptom[];
+}
+
+
+// SYMTOMS ENTITY AND TYPES 
+export type symptomsTypes = 
+    'cough' |
+    'soreThroat' |
+    'temperatureMorning' |
+    'temperatureEvening' |
+    'shortnessOfBreath' |
+    'runnyNose' |
+    'lossOfSmell' |
+    'headache' |
+    'abdominalPain' |
+    'vomiting' |
+    'bodyAches' |
+    'diarrhea';
+
+@Entity("Symptom")
+export class Symptom {
+    @PrimaryGeneratedColumn({ name: 'id', type: 'integer'})
+    id?: number;
+
+    @Column({ name: 'date', type: 'date', nullable: false})
+    date!: Date
+
+    @Column({ name: 'type', type: 'varchar', nullable: false})
+    type!: string
+
+    @Column({ name: 'value', type: 'integer', nullable: false})
+    value!: number
+
+    @ManyToOne(type => User, user => user.symptoms)
+    user?: User;
+
+}
