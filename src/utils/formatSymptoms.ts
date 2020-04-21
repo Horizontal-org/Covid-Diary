2fc9d3fa2edf9040fcf07e3964c54c0b45abf5c8
Marcos Gutierrez <gmarcos87@gmail.com>
2020-04-21 13:31:38 -0300

    refactor mergeSymptoms and add removeNull

--- src/utils/formatSymptoms.ts
+++ src/utils/formatSymptoms.ts
@@ -19,62 +19,66 @@ export type SymptomsList = {
 export type DayRecord = {
     date: Date;
     symptoms?: SymptomsList;
-    id: number;
+    id?: number;
 };
 
+export const mergeSymptoms = (prev: DayRecord[], act: Symptom): DayRecord[] => {
+  let day = prev.find(({ date }) => moment(date).isSame(act.date));
+  const type = act.type as symptomsTypes;
+  if (!day) {
+    const newDate: DayRecord = {
+      id: act.id ? act.id : Date.now(),
+      date: act.date,
+      symptoms: {
+        [type]: act.value,
+      },
+    };
+    return [...prev, newDate];
+  }
+  return prev.map((dayRecord) => {
+    if (dayRecord.date !== act.date) return dayRecord;
+    dayRecord.symptoms = {
+      ...dayRecord.symptoms,
+      [type]: act.value,
+    };
+    return dayRecord;
+  });
+}
+
+const addEmptyDays = (prev: DayRecord[], act: DayRecord, index: number, original:DayRecord[]): DayRecord[] => {
+  if (original.length === index + 1) return [...prev, act];
+  if (
+    moment(original[index + 1].date)
+      .subtract(1, "days")
+      .isSame(act.date)
+  )
+    return [...prev, act];
+  const daysUntilNextRecord = moment(act.date).diff(
+    original[index + 1].date,
+    "days"
+  );
+  let fillRecord: DayRecord[] = [];
+  for (let i = 1; i < daysUntilNextRecord; i++) {
+    fillRecord = [
+      ...fillRecord,
+      { id: 0, date: moment(act.date).subtract(i, "days").toDate() },
+    ];
+  }
+  fillRecord = fillRecord.map((record, index) => {
+    record.id = (act.id ? act.id : 999)  * 1000 + index;
+    return record;
+  });
+  return [...prev, act, ...fillRecord];
+};
+
+const sortByDate = (a: Symptom, b: Symptom) => moment(b.date).diff(a.date, "days");
+
+export const removeNull = (a: Symptom) => a.value !== 1;
 
 export const formatSymptoms = (userSymtoms: Symptom[]) => {
-    return userSymtoms.sort((a, b) => moment(b.date).diff(a.date, "days"))
-    .reduce((prev: DayRecord[], act): DayRecord[] => {
-      if (act.value === 1) return prev;
-      let day = prev.find(({ date }) => moment(date).isSame(act.date));
-      const type = act.type as symptomsTypes;
-      if (!day) {
-        const newDate: DayRecord = {
-          id: act.id ? act.id : Date.now(),
-          date: act.date,
-          symptoms: {
-            [type]: act.value,
-          },
-        };
-        return [...prev, newDate];
-      }
-      return prev.map((dayRecord) => {
-        if (dayRecord.date !== act.date) return dayRecord;
-        dayRecord.symptoms = {
-          ...dayRecord.symptoms,
-          [type]: act.value,
-        };
-        return dayRecord;
-      });
-    }, [])
-    //add empty days
-    .reduce(
-      (prev: DayRecord[], act: DayRecord, index, original): DayRecord[] => {
-        if (original.length === index + 1) return [...prev, act];
-        if (
-          moment(original[index + 1].date)
-            .subtract(1, "days")
-            .isSame(act.date)
-        )
-          return [...prev, act];
-        const daysUntilNextRecord = moment(act.date).diff(
-          original[index + 1].date,
-          "days"
-        );
-        let fillRecord: DayRecord[] = [];
-        for (let i = 1; i < daysUntilNextRecord; i++) {
-          fillRecord = [
-            ...fillRecord,
-            { id: 0, date: moment(act.date).subtract(i, "days").toDate() },
-          ];
-        }
-        fillRecord = fillRecord.map((record, index) => {
-          record.id = act.id * 1000 + index;
-          return record;
-        });
-        return [...prev, act, ...fillRecord];
-      },
-      []
-    )
+    return userSymtoms
+      .sort(sortByDate)
+      .reduce(mergeSymptoms, [])
+      .reduce(addEmptyDays, []);
+      
 }
